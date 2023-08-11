ac5e64935db644f6e9ee66aa2555be20e0abf73b
Joel Jucá <joelwallis@gmail.com>
2023-08-11 14:19:46 -0300

    chore: rm hello/0 from NimbleBlog

--- lib/nimble_blog.ex
+++ lib/nimble_blog.ex
@@ -2,17 +2,4 @@ defmodule NimbleBlog do
   @moduledoc """
   Documentation for `NimbleBlog`.
   """
-
-  @doc """
-  Hello world.
-
-  ## Examples
-
-      iex> NimbleBlog.hello()
-      :world
-
-  """
-  def hello do
-    :world
-  end
 end
